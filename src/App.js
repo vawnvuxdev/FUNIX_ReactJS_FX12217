c90961e42048a4881eb52a020f7dd1969def6f5a
vawnvuxdev <vanvu.nd.97@gmail.com>
2021-10-30 19:03:16 +0900

    Delete content in App.css, Delete init logo, delete exercise file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import "./App.css";
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-// import Menu from "./components/MenuComponsent";
-import StaffList from "./components/StaffListComponent";
-import { DISHES } from "./shared/dishes";
-import { STAFFS } from "./shared/staffs";
+import Main from "./components/MainComponent"; 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES,
-      staffs: STAFFS,
-    };
-  }
 
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container-fluid">
-            <NavbarBrand href="/">ReactJS - Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-          </div>
-        </Navbar>
-        {/* <Menu dishes={this.state.dishes}/> */}
-        <StaffList staffs={this.state.staffs} />
+        <Main />
       </div>
     );
   }
