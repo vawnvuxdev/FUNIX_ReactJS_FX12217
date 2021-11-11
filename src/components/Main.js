da78aa28f15301ba2da92fcbfdcb906df57232c7
VawnVuxDev <vanvu.nd.97@gmail.com>
2021-11-11 13:57:45 +0900

    Use redux, add redux folder to src, use Controlled Form and validate form to add new staff to stafflist, use Uncontrolled Form to search staff from stafflist in StaffList.js

---
+++ src/components/Main.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import StaffList from "./staff/StaffList";
+import StaffDetail from "./staff/StaffDetail";
+import DepartmentList from "./department/DepartmentList";
+import SalaryList from "./salary/SalaryList";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return {
+    staffs: state.staffs,
+    departments: state.departments
+  }
+}
+
+class Main extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    const HomePage = () => {
+      return (
+        <div className="text-center">
+          <h1>Welcome to ASsignment 02</h1>
+        </div>
+      );
+    };
+
+    const StaffDetailById = ({ match }) => {
+      return (
+        <StaffDetail
+          staff={
+            this.props.staffs.filter(
+              (staff) => staff.id === parseInt(match.params.staffId, 10)
+            )[0]
+          }
+        />
+      );
+    };
+
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route
+            exact
+            path="/staffs"
+            component={() => <StaffList staffs={this.props.staffs} departments={this.props.departments} />}
+          />
+          <Route path="/staffs/:staffId" component={StaffDetailById} />
+          <Route
+            exact
+            path="/departments"
+            component={() => (
+              <DepartmentList departments={this.props.departments} />
+            )}
+          />
+          <Route
+            exact
+            path="/salaries"
+            component={() => <SalaryList staffs={this.props.staffs} />}
+          />
+          <Redirect to="/" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(Main));
